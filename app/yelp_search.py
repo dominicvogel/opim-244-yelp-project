60385e1217308a20f43ad102ff6557b155c5a4df
sachitejani <skt39@georgetown.edu>
2022-12-04 22:19:47 -0500

    Simply and refactor code

--- app/yelp_search.py
+++ app/yelp_search.py
@@ -1,10 +1,49 @@
-from requests import cookies
 from copy import Error
 import requests
 import json
 from yelp_load import API_KEY
 
 # from IPython.display import Image, display 
+
+def fetch_latenight(ids, headers):
+# parse through the list of ids for businesses that are open overnight
+# use different request url to receive specific overnight parameters
+    late_night_options = []
+    
+    for id in ids:
+        specific_request_url = f"https://api.yelp.com/v3/businesses/{id}"
+        specific_response = requests.get(specific_request_url,headers=headers)
+        specific_data = json.loads(specific_response.text)
+        count = 0
+        try:
+            daily_hours = specific_data["hours"][0]["open"]
+            for day in daily_hours:
+                if day["is_overnight"]==True:
+                    count+=1
+                if count>=2:
+                    late_night_options.append(specific_data)
+                    break
+        # keyerrors arise when the hours are not listed for a location
+        except KeyError as e:
+            print()
+    
+    return late_night_options
+
+
+
+def fetch_reviews(id, headers):
+    review_url=f"https://api.yelp.com/v3/businesses/{id}/reviews"
+    review_response = requests.get(review_url,headers=headers)
+    review_data = json.loads(review_response.text)
+    countreviews = 1
+    for review in review_data["reviews"]:
+        print("REVIEW", countreviews, ":")
+        print(review["text"])
+        print()
+        countreviews+=1
+        if countreviews>3:
+            break
+
 
 def georgetown_yelp(TERM="sandwiches",LOCATION="georgetown"):
     request_url = f"https://api.yelp.com/v3/businesses/search?term={TERM}&location={LOCATION}"
@@ -13,81 +52,31 @@ def georgetown_yelp(TERM="sandwiches",LOCATION="georgetown"):
         }
     response = requests.get(request_url,headers=headers)
     data = json.loads(response.text)
-    # print(len(data["businesses"]))
-    # print(type(data))
-    # print(data.keys())
-    # print(data["businesses"][0])
-    # print(data["businesses"][0]["id"])
-
-    # list of ids for all search results that meet the parameters
+    
+    # create a list of ids for all search results that meet the parameters
     ids = []
     for business in data["businesses"]:
         ids.append(business["id"])
-    # print(ids)
-    # id = ids[0]
+
+    late_night_options = fetch_latenight(ids, headers)
     
-    # parse through the list of ids for businesses that are open overnight
-    # use different request url to receive specific overnight parameters
-    late_night_options = []
-    for id in ids:
-        specific_request_url = f"https://api.yelp.com/v3/businesses/{id}"
-        #review_url=f"https://api.yelp.com/v3/businesses/{id}/reviews"
-        response = requests.get(specific_request_url,headers=headers)
-        #review_response = requests.get(review_url,headers=headers)
-        data = json.loads(response.text)
-        #review_data = json.loads(review_response.text)
+    if len(late_night_options) == 0:
+        print("Sorry, no options found. What else are you craving?")
 
-
-        # check if the business is open overnight for each day of the week if it is for 3 or more days then
-        # we add to the late night options
-        count = 0
-        # print(data.keys())
-        try:
-            daily_hours = data["hours"][0]["open"]
-            # print(daily_hours)
-            for day in daily_hours:
-                if day["is_overnight"]==True:
-                    count+=1
-                if count>=2:
-                    late_night_options.append(data)
-                    break
-        # keyerrors arise when the hours are not listed for a location
-        except KeyError as e:
-            print()
-    # print(late_night_options)
     # display information from the late night options
-    print("Here are your late night options: ")
+    print("Here are your late night options in", LOCATION,": ")
     for option in late_night_options:
-        # print(option.keys())
         print("NAME: ", option["name"])
         print("RATING: ", option["rating"])
         print("LOCATION: ", option["location"]["display_address"])
-        print("PRICE: ", option["price"])
-        print()
-        reviewid=option["id"]
-        review_url=f"https://api.yelp.com/v3/businesses/{reviewid}/reviews"
-        review_response = requests.get(review_url,headers=headers)
-        review_data = json.loads(review_response.text)
-        # print(review_data)
-        countreviews = 1
-        # review_list = []
-        # print(review_data["reviews"])
-        for review in review_data["reviews"]:
-            print("REVIEW", countreviews, ":")
-            print(review["text"])
+        try:
+            print("PRICE: ", option["price"])
+        except KeyError as e:
             print()
-            countreviews+=1
-        #   review_list.append(review["text"])
-        #   countreviews+=1
-        #   if countreviews==5:
-        #     break
-
-        # print(review_list)
-        # print(review_data["reviews"][0]["text"])
+        print()
+        fetch_reviews(option["id"], headers)
 
 
-
-
-georgetown_yelp(TERM="crepes")
+georgetown_yelp(TERM="hummus")
 
 
