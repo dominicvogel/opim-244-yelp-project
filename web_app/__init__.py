522bb16c2d234febdf777f1e564215942e56e86b
sachitejani <111895054+sachitejani@users.noreply.github.com>
2022-12-07 10:59:30 -0500

    Web app (#6)
    * create cards in display template
    
    * fix location display

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -5,12 +5,16 @@ from flask import Flask
 from web_app.routes.home_routes import home_routes
 from web_app.routes.input_routes import input_routes
 # from web_app.routes.display_routes import display_routes
+# SECRET_KEY = os.getenv("SECRET_KEY", default="super secret")
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(home_routes)
     app.register_blueprint(input_routes)
     # app.register_blueprint(display_routes)
+
+    
+    # app.config["SECRET_KEY"] = SECRET_KEY
     return app
 
 if __name__ == "__main__":
