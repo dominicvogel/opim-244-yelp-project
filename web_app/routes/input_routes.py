cd6cf1c3e9f343a71d84e48e3d19518dd92602d8
sachitejani <skt39@georgetown.edu>
2022-12-07 01:31:48 -0500

    refactor yelp_search in app directory and create new route

---
+++ web_app/routes/input_routes.py
@@ -0,0 +1,63 @@
+
+# this is the "web_app/routes/stocks_routes.py" file ...
+
+from flask import Blueprint, request, render_template, redirect, flash
+
+from app.yelp_search import fetch_latenight, fetch_reviews, georgetown_yelp
+
+input_routes = Blueprint("input_routes", __name__)
+
+@input_routes.route("/input/form")
+def input_form():
+    print("INPUT FORM...")
+    return render_template("input_form.html")
+
+@input_routes.route("/input/display", methods=["GET", "POST"])
+def input_display():
+    print("DISPLAYING OPTIONS...")
+
+    if request.method == "POST":
+        # for data sent via POST request, form inputs are in request.form:
+        request_data = dict(request.form)
+        print("FORM DATA:", request_data)
+    else:
+        # for data sent via GET request, url params are in request.args
+        request_data = dict(request.args)
+        print("URL PARAMS:", request_data)
+    print(type(request_data))
+    term = request_data["term"]
+    location = request_data["location"]
+    print(term)
+    try:
+        ids = georgetown_yelp(term, location)
+        businesses = fetch_latenight(ids[0],ids[1])
+        #flash("Fetched Real-time Market Data!", "success")
+        return render_template("input_display.html",
+            businesses = businesses
+        )
+    except Exception as err:
+        print('OOPS', err)
+
+        #flash("Market Data Error. Please check your symbol and try again!", "danger")
+        return redirect("/input/form")
+
+#
+# API ROUTES
+#
+
+# @stocks_routes.route("/api/stocks.json")
+# def stocks_api():
+#     print("STOCKS DATA (API)...")
+
+#     # for data supplied via GET request, url params are in request.args:
+#     url_params = dict(request.args)
+#     print("URL PARAMS:", url_params)
+#     symbol = url_params.get("symbol") or "NFLX"
+
+#     try:
+#         df = fetch_stocks_data(symbol=symbol)
+#         data = df.to_dict("records")
+#         return {"symbol": symbol, "data": data }
+#     except Exception as err:
+#         print('OOPS', err)
+#         return {"message":"Market Data Error. Please try again."}, 404
